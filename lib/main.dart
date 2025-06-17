2c39206f584bb8270d15eb7f7b0be7c36dac62f3
LunaBOY65 <mooyong310103@gmail.com>
2025-06-17 23:27:57 +0700

    Topic282 : Adding SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'package:section14_chatapp/screens/chat.dart';
+import 'package:section14_chatapp/screens/splash.dart';
 
 import 'firebase_options.dart';
 import 'package:section14_chatapp/screens/auth.dart';
@@ -28,6 +30,10 @@ class App extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const SplashScreen();
+          }
+
           if(snapshot.hasData){
             return const ChatScreen();
           }
